ba2a2de8548434db93a18674c2682f7aaf020017
silpa757krishna <silpa.unnikrishnan@hexahives.com>
2024-11-20 17:11:34 +0530

    package added

---
+++ lib/models/product.dart
@@ -0,0 +1,12 @@
+class Product {
+  Product(
+      {required this.id,
+      required this.tittle,
+      required this.price,
+      required this.image});
+
+  final String id;
+  final String tittle;
+  final String image;
+  final int price;
+}
